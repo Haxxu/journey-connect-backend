82398a0a568e6798a7a706310c26d0e359b599e2
Minh Nguyen <nguyenngocminh190901@gmail.com>
2023-10-24 18:05:55 +0700

    add getUsers service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -21,6 +21,49 @@ class UserService {
 			access_token,
 		};
 	}
+
+	static async getUsers(condition: any) {
+		try {
+			let search = condition.search as string;
+			let page = condition.page as string;
+			let pageSize = condition.pageSize as string;
+			let createdStartDate = condition.createdStartDate as string;
+			let createdEndDate = condition.createdEndDate as string;
+
+			const query: any = {};
+			if (search) {
+				query.$or = [
+					{ username: { $regex: new RegExp(search, 'i') } },
+					{ email: { $regex: new RegExp(search, 'i') } },
+					{ first_name: { $regex: new RegExp(search, 'i') } },
+					{ last_name: { $regex: new RegExp(search, 'i') } },
+				];
+			}
+			if (createdStartDate && createdEndDate) {
+				query.createdAt = {
+					$gte: new Date(createdStartDate),
+					$lte: new Date(createdEndDate),
+				};
+			} else if (createdStartDate) {
+				query.createdAt = { $gte: new Date(createdStartDate) };
+			} else if (createdEndDate) {
+				query.createdAt = { $lte: new Date(createdEndDate) };
+			}
+
+			const options = {
+				page: parseInt(page, 10) || 1,
+				limit: parseInt(pageSize, 10) || 10,
+				sort: { createdAt: -1 },
+				select: '-password',
+			};
+
+			const users = await User.paginate(query, options);
+
+			return users;
+		} catch (error) {
+			console.error('Error get user documents:', error);
+		}
+	}
 }
 
 export default UserService;
